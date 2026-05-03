3a8a33faa38d05f7a91063e715cb1bedb71fd747
AlexLa <languyenphuchung07@gmail.com>
2026-05-03 07:17:11 -0700

    newCode

--- home.js
+++ home.js
@@ -1,6 +1,15 @@
 import { auth } from "../Firebase/firebase.js"; 
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
 
+
+
+
+
+
+
+
+
+
 // Đặt thời gian hết hạn là 20 phút
 const SESSION_TIMEOUT_MS = 20 * 60 * 1000; 
 let logoutTimer; 
@@ -79,3 +88,45 @@ if (logoutBtn) {
         });
     });
 }
+
+async function init() {
+  await customElements.whenDefined('gmp-map');
+
+  const map = document.querySelector('gmp-map');
+  const marker = document.querySelector('gmp-advanced-marker');
+  const placePicker = document.querySelector('gmpx-place-picker');
+  const infowindow = new google.maps.InfoWindow();
+
+  map.innerMap.setOptions({
+    mapTypeControl: false
+  });
+
+  placePicker.addEventListener('gmpx-placechange', () => {
+    const place = placePicker.value;
+
+    if (!place.location) {
+      window.alert(
+        "No details available for input: '" + place.name + "'"
+      );
+      infowindow.close();
+      marker.position = null;
+      return;
+    }
+
+    if (place.viewport) {
+      map.innerMap.fitBounds(place.viewport);
+    } else {
+      map.center = place.location;
+      map.zoom = 17;
+    }
+
+    marker.position = place.location;
+    infowindow.setContent(
+  `<strong>${place.displayName}</strong><br>
+   <span>${place.formattedAddress}</span>`
+);
+    infowindow.open(map.innerMap, marker);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', init);
